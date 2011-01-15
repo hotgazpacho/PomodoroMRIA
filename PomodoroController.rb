8a409d708050dd39793dee3c313afee282b04194
Will Green <will@hotgazpacho.org>
2011-01-15 17:10:11 -0500

    Got the app working :-) Still an issue with NSSpeechSynthesizer and undefined method `startSpeekingString'

--- PomodoroController.rb
+++ PomodoroController.rb
@@ -7,15 +7,19 @@
 class PomodoroController 
 	attr_accessor :timer_label, :start_stop_button 
 	attr_accessor :pomodoro_timer, :time_left
+	attr_accessor :minutes, :warn_limit, :alert_limit
 	
 	def awakeFromNib()
-		@time_left = 25
+		@minutes = 1
+		@time_left = @minutes * 60
+    @warn_limit = @time_left * 2/5
+    @alert_limit = @time_left * 1/5
+    set_timer_label(@time_left)
 	end
 	
-	def start_stop_timer(sender)
+	def start_stop_pomodoro(sender)
 		if @pomodoro_timer.nil?
-			@pomodoro_timer = NSTimer.scheduledTimerWithTimeInterval(60, [ CA]target:self, selector:"time_passed:", userInfo:nil, repeats:true) 
-			@timer_label.textColor = NSColor.redColor 
+			@pomodoro_timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"time_passed:", userInfo:nil, repeats:true) 
 			@start_stop_button.title = "Stop Pomodoro"
 		else 
 			reset_interface
@@ -25,25 +29,46 @@ class PomodoroController
 	def time_passed(timer)
 		if @time_left > 1 
 			@time_left -= 1
-			@timer_label.stringValue = @time_left
+      set_timer_label(@time_left)
 		else
 			reset_interface
-			timer_finished_alert 
+			alert_user 
 		end
 	end
-	
-	def reset_interface 
-		@time_left = 25 
-		@timer_label.stringValue = @time_left 
-		@timer_label.textColor = NSColor.blackColor 
+  
+  def reset_interface 
+		@time_left = @minutes * 60 
+    set_timer_label(@time_left)
 		@start_stop_button.title = "Start Pomodoro" 
 		@pomodoro_timer.invalidate 
 		@pomodoro_timer = nil
 	end
+  
+  protected
+  def set_timer_label(time_left)
+    @timer_label.textColor = label_color(time_left)
+    minutes, seconds = minutes_and_seconds @time_left
+    @timer_label.stringValue = format_time(minutes, seconds)
+  end
+  
+  def label_color(time_left)
+    color = NSColor.blackColor
+    color = NSColor.yellowColor if time_left < @warn_limit
+    color = NSColor.redColor if time_left < @alert_limit
+    color
+  end
+	
+	def minutes_and_seconds(time_left)
+		[@time_left/60 % 60, @time_left % 60]
+	end
+	
+	def format_time(minutes, seconds)
+		"#{minutes.to_s.rjust(2,'0')}:#{seconds.to_s.rjust(2,'0')}"
+	end
 	
 	def alert_user 
-		voice = NSSpeechSynthesizer.alloc.[ CA]initWithVoice("com.apple.speech.synthesis.voice.Victoria") 
-		voice.startSpeekingString("Pomodoro complete. Time for a short break")
+		voice = NSSpeechSynthesizer.alloc.initWithVoice("com.apple.speech.synthesis.voice.Victoria") 
+		voice.startSpeekingString("Pomodoro complete! Time for a short break.")
 	end
 	
 end
