4fb83f0fefc7d39e1c77f59464ad866d6e0e8653
will <will@hotgazpacho.org>
2011-01-15 09:03:33 -0500

    Create project in XCode

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PomodoroMRIA
+//
+//  Created by Will Green on 1/15/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
